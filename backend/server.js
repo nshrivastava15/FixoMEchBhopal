16b4ea94260f593729e7c38e9ae1f46ba028c1d8
nshrivastava15 <nshrivastava15@gmail.com>
2021-06-12 05:45:06 +0530

    first commit

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import data from './data.js';
+const app= express();
+
+app.get('/api/products',(req,res)=>{
+    res.send(data.products);
+})
+app.get('/', (req,res)=>{
+    res.send("Server is Ready");
+});
+const port = process.env.PORT || 5000
+app.listen(port,()=>{
+    console.log(`Server started at ${port}`);
+});
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react';
+import {BrowserRouter, Route} from 'react-router-dom';
+import ProductScreen from './screens/ProductScreen';
+import HomeScreen from './screens/HomeScreen';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <BrowserRouter>
+  <div className="grid-container">
+    <header className="row">
+      <div>
+        <a className="brand" href="/">FIXOMECH</a>
+      </div>
+      <div>
+        <a href="/cart">Cart</a>
+        <a href="/signin">Sign In</a>
+      </div>
+    </header>
+    <main>
+      <Route path="/product/:id" component={ProductScreen}></Route>
+      <Route path="/" component={HomeScreen} exact></Route>
+    </main>
+    <footer className="row center">All right reserved</footer>
+  </div>
+  </BrowserRouter>
   );
 }
 
