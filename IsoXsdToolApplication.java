0fd4c89a88822316d2a49ab52a16a513a52e56d6
nshrivastava15 <31294045+nshrivastava15@users.noreply.github.com>
2025-11-27 23:18:44 +0000

    Create IsoXsdToolApplication.java

---
+++ IsoXsdToolApplication.java
@@ -0,0 +1,11 @@
+package com.example.isoxsd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsoXsdToolApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(IsoXsdToolApplication.class, args);
+  }
+}
